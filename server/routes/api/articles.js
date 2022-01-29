c867575b4eb6d163fb753a71c53fc3eb27aa5e33
jonklinger02 <jon.klinger02@gmail.com>
2022-01-29 16:34:23 -0500

    next is pagination

--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -27,8 +27,78 @@ router
   );
 
 //admin get,patch,delete, single article(draft or public)
+router
+  .route("/admin/:id")
+  .get(checkLoggedIn, grantAccess("readAny", "article"), async (req, res) => {
+    try {
+      const _id = req.params.id;
+      const article = await Article.findById(_id);
+
+      if (!article || article.length === 0) {
+        return res.status(400).json({ message: "Article not found." });
+      }
+
+      res.status(200).json(article);
+    } catch (error) {
+      res.status(400).json({ message: "Error fetching article.", error });
+    }
+  })
+  .patch(
+    checkLoggedIn,
+    grantAccess("updateAny", "article"),
+    async (req, res) => {
+      try {
+        const _id = req.params.id;
+        const article = await Article.findOneAndUpdate(
+          { _id },
+          { $set: req.body },
+          { new: true }
+        );
+        if (!article) {
+          return res.status(400).json({ message: "Article not found." });
+        }
+
+        res.status(200).json(article);
+      } catch (error) {
+        res.status(400).json({ message: "error updating article", error });
+      }
+    }
+  )
+  .delete(
+    checkLoggedIn,
+    grantAccess("deleteAny", "article"),
+    async (req, res) => {
+      try {
+        const _id = req.params.id;
+        const article = await Article.findByIdAndDelete({ _id });
+
+        if (!article) {
+          return res.status(400).json({ message: "article not found" });
+        }
+        res.status(200).json(article);
+      } catch (error) {
+        res.status(400).json({ message: "error deleting article", error });
+      }
+    }
+  );
 //get articles no auth
+
+router.route("/getby_id/:id").get(async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const article = await Article.find({ _id, status: "public" });
+    if (!article || article.length === 0) {
+      return res.status(400).json({ message: "Article not found." });
+    }
+
+    res.status(200).json(article);
+  } catch (error) {
+    res.status(400).json({ message: "Error fetching article.", error });
+  }
+});
+
 //fetch articles load more
+
 //fetch articles with pagination
 
 module.exports = router;
